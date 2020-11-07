66e442ef5f03f9ffb199091bd358ef3b799c4b43
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-11-06 22:30:26 -0500

    Add nav drawer

--- app/src/main/java/com/sullivan/example/MainActivity.kt
+++ app/src/main/java/com/sullivan/example/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.sullivan.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(navView, getNavController())
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this, getNavController(), drawerLayout)
+
+        // Attempt to remove back arrow from navigation bar
+        // this works, however still does the back action and doesnt open the navDrawer
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(R.id.startFragment, R.id.homeFragment), drawerLayout)
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(getNavController(), drawerLayout)
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        return navHostFragment.navController
     }
 }
